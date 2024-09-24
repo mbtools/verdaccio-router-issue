f6a05e049448b70d2f5b5e9b1ca7574df37d78c3
Marc Bernard <59966492+mbtools@users.noreply.github.com>
2024-09-24 09:38:45 -0400

    Update

--- test/test.sh
+++ test/test.sh
@@ -4,22 +4,32 @@ PROXY=$1
 
 rm -f test.log
 
-curl -s -X GET $PROXY/lodash >> test.log
-curl -s -X GET $PROXY/lodash/4.17.21 >> test.log
-curl -s -X GET $PROXY/lodash/-/lodash-4.17.21.tgz >> test.log
+echo ### Test 1: Get regular package > test.log
 
-curl -s -X GET $PROXY/@types/lodash >> test.log
-curl -s -X GET $PROXY/@types/lodash/4.17.9 >> test.log
-curl -s -X GET $PROXY/@types/lodash/-/lodash-4.17.9.tgz >> test.log
+curl $PROXY/lodash >> test.log
+curl $PROXY/lodash/4.17.21 >> test.log
+curl $PROXY/lodash/-/lodash-4.17.21.tgz >> test.log
 
-curl -s -X GET $PROXY/@types%2Flodash >> test.log
-curl -s -X GET $PROXY/@types%2Flodash/4.17.9 >> test.log
-curl -s -X GET $PROXY/@types%2Flodash/-/lodash-4.17.9.tgz >> test.log
+echo ### Test 2: Get scoped package >> test.log
 
-curl -s -X GET $PROXY/%40types/lodash >> test.log
-curl -s -X GET $PROXY/%40types/lodash/4.17.9 >> test.log
-curl -s -X GET $PROXY/%40types/lodash/-/lodash-4.17.9.tgz >> test.log
+curl $PROXY/@types/lodash >> test.log
+curl $PROXY/@types/lodash/4.17.9 >> test.log
+curl $PROXY/@types/lodash/-/lodash-4.17.9.tgz >> test.log
 
-curl -s -X GET $PROXY/%40types%2Flodash >> test.log
-curl -s -X GET $PROXY/%40types%2Flodash/4.17.9 >> test.log
-curl -s -X GET $PROXY/%40types%2Flodash/-/lodash-4.17.9.tgz >> test.log
+echo ### Test 3: Get scoped package escaped "slash" >> test.log
+
+curl $PROXY/@types%2Flodash >> test.log
+curl $PROXY/@types%2Flodash/4.17.9 >> test.log
+curl $PROXY/@types%2Flodash/-/lodash-4.17.9.tgz >> test.log
+
+echo ### Test 4: Get scoped package escaped "at" >> test.log
+
+curl $PROXY/%40types/lodash >> test.log
+curl $PROXY/%40types/lodash/4.17.9 >> test.log
+curl $PROXY/%40types/lodash/-/lodash-4.17.9.tgz >> test.log
+
+echo ### Test 5: Get scoped package escaped "at" and "slash" >> test.log
+
+curl $PROXY/%40types%2Flodash >> test.log
+curl $PROXY/%40types%2Flodash/4.17.9 >> test.log
+curl $PROXY/%40types%2Flodash/-/lodash-4.17.9.tgz >> test.log
