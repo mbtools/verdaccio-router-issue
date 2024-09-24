d181cbb2e40f15b835fa2d306ff3aa191afc31fb
Marc Bernard <59966492+mbtools@users.noreply.github.com>
2024-09-24 09:47:45 -0400

    Update tests

--- test/test.sh
+++ test/test.sh
@@ -6,30 +6,60 @@ rm -f test.log
 
 echo ### Test 1: Get regular package > test.log
 
-curl $PROXY/lodash >> test.log
-curl $PROXY/lodash/4.17.21 >> test.log
-curl $PROXY/lodash/-/lodash-4.17.21.tgz >> test.log
+$URL=$PROXY/lodash
+echo $URL >> test.log
+curl -s -o /dev/null -w "%{http_code}" $URL >> test.log
+$URL=$PROXY/lodash/4.17.21 
+echo $URL >> test.log
+curl -s -o /dev/null -w "%{http_code}" $URL >> test.log
+$URL=$PROXY/lodash/-/lodash-4.17.21.tgz
+echo $URL >> test.log
+curl -s -o /dev/null -w "%{http_code}" $URL >> test.log
 
 echo ### Test 2: Get scoped package >> test.log
 
-curl $PROXY/@types/lodash >> test.log
-curl $PROXY/@types/lodash/4.17.9 >> test.log
-curl $PROXY/@types/lodash/-/lodash-4.17.9.tgz >> test.log
+$URL=$PROXY/@types/lodash
+echo $URL >> test.log
+curl -s -o /dev/null -w "%{http_code}" $URL >> test.log
+$URL=$PROXY/@types/lodash/4.17.9
+echo $URL >> test.log
+curl -s -o /dev/null -w "%{http_code}" $URL >> test.log
+$URL=$PROXY/@types/lodash/-/lodash-4.17.9.tgz
+echo $URL >> test.log
+curl -s -o /dev/null -w "%{http_code}" $URL >> test.log
 
 echo ### Test 3: Get scoped package escaped "slash" >> test.log
 
-curl $PROXY/@types%2Flodash >> test.log
-curl $PROXY/@types%2Flodash/4.17.9 >> test.log
-curl $PROXY/@types%2Flodash/-/lodash-4.17.9.tgz >> test.log
+$URL=$PROXY/@types%2Flodash
+echo $URL >> test.log
+curl -s -o /dev/null -w "%{http_code}" $URL >> test.log
+$URL=$PROXY/@types%2Flodash/4.17.9
+echo $URL >> test.log
+curl -s -o /dev/null -w "%{http_code}" $URL >> test.log
+$URL=$PROXY/@types%2Flodash/-/lodash-4.17.9.tgz
+echo $URL >> test.log
+curl -s -o /dev/null -w "%{http_code}" $URL >> test.log
 
 echo ### Test 4: Get scoped package escaped "at" >> test.log
 
-curl $PROXY/%40types/lodash >> test.log
-curl $PROXY/%40types/lodash/4.17.9 >> test.log
-curl $PROXY/%40types/lodash/-/lodash-4.17.9.tgz >> test.log
+$URL=$PROXY/%40types/lodash
+echo $URL >> test.log
+curl -s -o /dev/null -w "%{http_code}" $URL >> test.log
+$URL=$PROXY/%40types%/lodash/4.17.9
+echo $URL >> test.log
+curl -s -o /dev/null -w "%{http_code}" $URL >> test.log
+$URL=$PROXY/%40types/lodash/-/lodash-4.17.9.tgz
+echo $URL >> test.log
+curl -s -o /dev/null -w "%{http_code}" $URL >> test.log
 
 echo ### Test 5: Get scoped package escaped "at" and "slash" >> test.log
 
-curl $PROXY/%40types%2Flodash >> test.log
-curl $PROXY/%40types%2Flodash/4.17.9 >> test.log
-curl $PROXY/%40types%2Flodash/-/lodash-4.17.9.tgz >> test.log
+$URL=$PROXY/%40types%2Flodash
+echo $URL >> test.log
+curl -s -o /dev/null -w "%{http_code}" $URL >> test.log
+$URL=$PROXY/%40types%2Flodash/4.17.9
+echo $URL >> test.log
+curl -s -o /dev/null -w "%{http_code}" $URL >> test.log
+$URL=$PROXY/%40types%2Flodash/-/lodash-4.17.9.tgz
+echo $URL >> test.log
+curl -s -o /dev/null -w "%{http_code}" $URL >> test.log
