38c753859cc6aff56261930ab5b948dda75a8c98
Marc Bernard <59966492+mbtools@users.noreply.github.com>
2024-09-26 20:59:21 -0400

    reverse

--- proxy/proxy.sh
+++ proxy/proxy.sh
@@ -10,7 +10,7 @@ rm -f debug.log
 # Boot the server in a background process
 # nohup npx verdaccio@latest --config ./config.yml --listen $TEST_CASE_PROXY &
 # nohup env DEBUG="express:*,verdaccio:storage*,verdaccio:proxy*,verdaccio:plugin:local-storage*,verdaccio:api*" npx verdaccio@next-7 --config ./config.yml --listen $TEST_CASE_PROXY >debug.log 2>error.log &
-nohup env DEBUG=$DEBUG npx @mbtools/verdaccio-dev@$VERSION --config ./config.yml --listen $PROXY 2> debug.log &
+nohup env DEBUG=$DEBUG npx verdaccio@$VERSION --config ./config.yml --listen $PROXY 2> debug.log &
 
 # Wait for the server to begin listening for connections
 ( tail -F -n5 proxy.log & ) | fgrep -q $PROXY
